78bcea1aa2e95217eb6f87e8fae12aa4239a21b5
Daniel Stirba <danielstirba97@gmail.com>
2022-08-11 02:52:04 +1000

    Added Ore to world gen and added crafting

--- src/main/java/net/fabricmc/example/ItemRegister.java
+++ src/main/java/net/fabricmc/example/ItemRegister.java
@@ -1,15 +1,44 @@
 package net.fabricmc.example;
 
+import java.util.Arrays;
+
 import eu.pb4.polymer.ext.blocks.api.BlockModelType;
+import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
+import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
 import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.BuiltinRegistries;
 import net.minecraft.util.registry.Registry;
+import net.minecraft.util.registry.RegistryEntry;
+import net.minecraft.util.registry.RegistryKey;
+import net.minecraft.world.gen.GenerationStep;
+import net.minecraft.world.gen.YOffset;
+import net.minecraft.world.gen.feature.ConfiguredFeature;
+import net.minecraft.world.gen.feature.Feature;
+import net.minecraft.world.gen.feature.OreConfiguredFeatures;
+import net.minecraft.world.gen.feature.OreFeatureConfig;
+import net.minecraft.world.gen.feature.PlacedFeature;
+import net.minecraft.world.gen.placementmodifier.CountPlacementModifier;
+import net.minecraft.world.gen.placementmodifier.HeightRangePlacementModifier;
+import net.minecraft.world.gen.placementmodifier.SquarePlacementModifier;
 
 public class ItemRegister {
+
+    public static final Block KYBER_ORE = regKyberOre(BlockModelType.FULL_BLOCK, "kyber_ore");
+
+    private static ConfiguredFeature<?, ?> KYBER_ORE_CONFIGURED_FEATURE = new ConfiguredFeature(Feature.ORE,
+            new OreFeatureConfig(OreConfiguredFeatures.NETHERRACK, KYBER_ORE.getDefaultState(), 4));
+
+    public static PlacedFeature KYBER_ORE_PLACED_FEATURE = new PlacedFeature(
+            RegistryEntry.of(KYBER_ORE_CONFIGURED_FEATURE), Arrays.asList(
+                    CountPlacementModifier.of(20),
+                    SquarePlacementModifier.of(),
+                    HeightRangePlacementModifier.uniform(YOffset.getBottom(), YOffset.fixed(64))));
 
     private static void regTestArmor(EquipmentSlot slot, String main, String id) {
         Registry.register(Registry.ITEM, new Identifier("test", main + "_" + id), new TestArmor(slot,
@@ -38,13 +67,15 @@ public class ItemRegister {
                         new Identifier("polymertest", "item/" + main + "_" + id)));
     }
 
-    public static void regKyberOre(BlockModelType type, String modelId) {
+    public static Block regKyberOre(BlockModelType type, String modelId) {
         var id = new Identifier("test", modelId);
         var block = Registry.register(Registry.BLOCK, id,
                 new KyberOre(FabricBlockSettings.copy(Blocks.DIAMOND_ORE), type, "block/" + modelId));
 
         Registry.register(Registry.ITEM, id,
                 new KyberOreItem(new Item.Settings().group(ItemGroup.BUILDING_BLOCKS), block, "block/" + modelId));
+
+        return block;
     }
 
     public static void register() {
@@ -81,8 +112,13 @@ public class ItemRegister {
         regSaberParts("kyber", "yellow");
         regSaberParts("kyber", "orange");
 
-        // Register Ores
-        regKyberOre(BlockModelType.FULL_BLOCK, "kyber_ore");
+        Registry.register(BuiltinRegistries.CONFIGURED_FEATURE, new Identifier("test", "kyber_ore"),
+                KYBER_ORE_CONFIGURED_FEATURE);
+        Registry.register(BuiltinRegistries.PLACED_FEATURE, new Identifier("test", "kyber_ore"),
+                KYBER_ORE_PLACED_FEATURE);
+        BiomeModifications.addFeature(BiomeSelectors.foundInTheNether(), GenerationStep.Feature.UNDERGROUND_ORES,
+                RegistryKey.of(Registry.PLACED_FEATURE_KEY, new Identifier("test", "kyber_ore")));
+
     }
 
 }
